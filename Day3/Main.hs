2e151769fdb99cb6ba7ef53c58be0600ab5df6e0
Erik Rasmussen <ecr.referee@hotmail.com>
2021-12-02 23:32:53 -0600

    WIP

--- Day3/Main.hs
+++ Day3/Main.hs
@@ -3,13 +3,13 @@ module Main where
 import Text.Printf (printf)
 
 part1 :: [String] -> Int
-part1 _ = 0
+part1 input = 0
 
 part2 :: [String] -> Int
 part2 _ = 0
 
 main :: IO ()
 main = do
-  input <- lines <$> readFile "./input.txt"
-  printf "Part1: %d" (part1 input)
-  printf "Part2: %d" (part2 input)
+  input <- lines <$> readFile "./test.txt"
+  printf "Part1: %d\n" (part1 input)
+  printf "Part2: %d\n" (part2 input)
